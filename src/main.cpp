2fc3c6848efbb898340362714041f08a0adfb13f
RogerHdzC <rogeliohernandezcortes573@gmail.com>
2021-11-26 14:33:43 -0600

    ESTRUCTURA

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
+/*
+PROYECTO:
+TIENDA DE VIDEJOUEGOS ONLINE
+ROGELIO HERNÁNDEZ CORTÉS A01735819
+DIEGO
+CHRISTIAN
+*/
 
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+
+
+
+  return 0;
+}
